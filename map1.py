94fbbd1371a552123cf5ffdc229eb5ed8dc6cf20
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2025-06-08 13:08:14 -0600

    added elevation in the popup for our marker. saved and recomplied.

--- map1.py
+++ map1.py
@@ -4,14 +4,15 @@ import pandas
 data = pandas.read_csv("Volcanoes.txt")
 lat = list(data["LAT"])
 lon = list(data["LON"])
+elev = list(data["ELEV"])
 
 
 map = folium.Map(location=[40.40, -104.73], zoom_start = 6, titles = "Stamen Terrain")
 
 fg = folium.FeatureGroup(name="Map")
 
-for lt, ln in zip(lat, lon):
-    fg.add_child(folium.Marker(location = [lt, ln], popup = "Volcaneo", icon = folium.Icon(color = 'black')))
+for lt, ln, el in zip(lat, lon, elev):
+    fg.add_child(folium.Marker(location = [lt, ln], popup = el, icon = folium.Icon(color = 'black')))
 
 #for coordinates in [[40.40, -104.73], [39.7392, -104.99],[40.585258, -105.084419], [40.397789, -105.075066], [40.014984, -105.270546]]:
    # fg.add_child(folium.Marker(location = coordinates, popup = "Location", icon = folium.Icon(color = 'black')))
