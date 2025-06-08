d415c604f5a6a3cf0f0163a766eb8539176f6b50
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2025-06-08 10:00:32 -0600

    added another marker for Denver just to test. Changed the icon colors for the markers

--- map1.py
+++ map1.py
@@ -3,7 +3,8 @@ import folium
 map = folium.Map(location=[40.40, -104.73], zoom_start = 6, titles = "Stamen Terrain")
 
 fg = folium.FeatureGroup(name="Map")
-fg.add_child(folium.Marker(location=[40.40, -104.73], popup = "Welcome!", icon = folium.Icon(color = 'green')))
+fg.add_child(folium.Marker(location=[40.40, -104.73], popup = "Greeley", icon = folium.Icon(color = 'beige')))
+fg.add_child(folium.Marker(location=[39.7392, -104.99], popup = "Denver", icon = folium.Icon(color = 'black')))
 map.add_child(fg)
 
 map.save("Map1.html")
