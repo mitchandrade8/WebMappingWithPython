a44b211952d568869ff6fe77a771fe62c22f7ff9
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2025-06-08 14:35:19 -0600

    added another layer. polygons for countries

--- map1.py
+++ map1.py
@@ -17,9 +17,7 @@ def color_producer(elevation):
 
 
 map = folium.Map(location=[40.40, -104.73], zoom_start = 6, titles = "Stamen Terrain")
-
 fg = folium.FeatureGroup(name="Map")
-
 for lt, ln, el in zip(lat, lon, elev):
     fg.add_child(folium.CircleMarker(location = [lt, ln], popup = str(el)+"m", fill_color = color_producer(el), color = 'grey', fill_opacity = 0.7))
 
@@ -28,8 +26,7 @@ for lt, ln, el in zip(lat, lon, elev):
 #fg.add_child(folium.Marker(location=[40.40, -104.73], popup = "Greeley", icon = folium.Icon(color = 'beige')))
 #fg.add_child(folium.Marker(location=[39.7392, -104.99], popup = "Denver", icon = folium.Icon(color = 'black')))
 
-fg.add_child(folium.GeoJson())
+fg.add_child(folium.GeoJson(data = (open('world.json', 'r', encoding='utf-8-sig').read())))
 
 map.add_child(fg)
-
 map.save("Map1.html")
