6e435fdb19b890909dd8388daa1612b093132056
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2025-06-08 15:58:37 -0600

    created to selected/deselectable layers : volcanoes and they are color cordinated and then population also color coordinated

--- map1.py
+++ map1.py
@@ -17,21 +17,23 @@ def color_producer(elevation):
 
 
 map = folium.Map(location=[40.40, -104.73], zoom_start = 6, titles = "Stamen Terrain")
-fg = folium.FeatureGroup(name="Map")
+
+fgv = folium.FeatureGroup(name="Volcanoes")
 for lt, ln, el in zip(lat, lon, elev):
-    fg.add_child(folium.CircleMarker(location = [lt, ln], popup = str(el)+"m", fill_color = color_producer(el), color = 'grey', fill_opacity = 0.7))
+    fgv.add_child(folium.CircleMarker(location = [lt, ln], popup = str(el)+"m", fill_color = color_producer(el), color = 'grey', fill_opacity = 0.7))
 
 #for coordinates in [[40.40, -104.73], [39.7392, -104.99],[40.585258, -105.084419], [40.397789, -105.075066], [40.014984, -105.270546]]:
    # fg.add_child(folium.Marker(location = coordinates, popup = "Location", icon = folium.Icon(color = 'black')))
 #fg.add_child(folium.Marker(location=[40.40, -104.73], popup = "Greeley", icon = folium.Icon(color = 'beige')))
 #fg.add_child(folium.Marker(location=[39.7392, -104.99], popup = "Denver", icon = folium.Icon(color = 'black')))
 
-fg.add_child(folium.GeoJson(data=(open('world.json', 'r', encoding='utf-8-sig').read()),
+fgp = folium.FeatureGroup(name= "Population")
+
+fgp.add_child(folium.GeoJson(data=(open('world.json', 'r', encoding='utf-8-sig').read()),
                            style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000 else 'orange' if x['properties']['POP2005'] < 20000000 else 'red'}))
 
-
-map.add_child(fg)
+map.add_child(fgv)
+map.add_child(fgp)
 map.add_child(folium.LayerControl())
-
 
 map.save("Map1.html")
