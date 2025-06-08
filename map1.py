5f6696117da0afdad4c8a537d01adab2778d1eb2
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2025-06-08 15:52:59 -0600

    added version 1 layer control but is missing. or not handling all layers correctly. Have 2 layers lumped with the current on/off status

--- map1.py
+++ map1.py
@@ -29,5 +29,9 @@ for lt, ln, el in zip(lat, lon, elev):
 fg.add_child(folium.GeoJson(data=(open('world.json', 'r', encoding='utf-8-sig').read()),
                            style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000 else 'orange' if x['properties']['POP2005'] < 20000000 else 'red'}))
 
+
 map.add_child(fg)
+map.add_child(folium.LayerControl())
+
+
 map.save("Map1.html")
