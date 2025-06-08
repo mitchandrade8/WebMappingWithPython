8d50c5b06b424ed4570cf53b8a3aa377331cb312
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2025-06-08 09:54:09 -0600

    refactoring to keep code clean and when we add more layer functionality

--- map1.py
+++ map1.py
@@ -2,6 +2,8 @@
 import folium
 map = folium.Map(location=[40.40, -104.73], zoom_start = 6, titles = "Stamen Terrain")
 
-map.add_child(folium.Marker(location=[40.40, -104.73], popup = "Welcome!", icon = folium.Icon(color = 'green')))
+fg = folium.FeatureGroup(name="Map")
+fg.add_child(folium.Marker(location=[40.40, -104.73], popup = "Welcome!", icon = folium.Icon(color = 'green')))
+map.add_child(fg)
 
 map.save("Map1.html")
