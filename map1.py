b8398970c28ff1235aaa1a6c3320dd11909a8a9a
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2025-06-08 10:05:37 -0600

    refactoring more!

--- map1.py
+++ map1.py
@@ -3,8 +3,11 @@ import folium
 map = folium.Map(location=[40.40, -104.73], zoom_start = 6, titles = "Stamen Terrain")
 
 fg = folium.FeatureGroup(name="Map")
-fg.add_child(folium.Marker(location=[40.40, -104.73], popup = "Greeley", icon = folium.Icon(color = 'beige')))
-fg.add_child(folium.Marker(location=[39.7392, -104.99], popup = "Denver", icon = folium.Icon(color = 'black')))
+
+for coordinates in [[40.40, -104.73], [39.7392, -104.99]]:
+    fg.add_child(folium.Marker(location = coordinates, popup = "Location", icon = folium.Icon(color = 'black')))
+#fg.add_child(folium.Marker(location=[40.40, -104.73], popup = "Greeley", icon = folium.Icon(color = 'beige')))
+#fg.add_child(folium.Marker(location=[39.7392, -104.99], popup = "Denver", icon = folium.Icon(color = 'black')))
 map.add_child(fg)
 
 map.save("Map1.html")
