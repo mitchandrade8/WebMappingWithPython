ac9b623b192a8b36899629eae8e132069466619b
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2025-06-08 10:13:18 -0600

    added more sample cities just for visuals

--- map1.py
+++ map1.py
@@ -4,7 +4,7 @@ map = folium.Map(location=[40.40, -104.73], zoom_start = 6, titles = "Stamen Ter
 
 fg = folium.FeatureGroup(name="Map")
 
-for coordinates in [[40.40, -104.73], [39.7392, -104.99]]:
+for coordinates in [[40.40, -104.73], [39.7392, -104.99],[40.585258, -105.084419], [40.397789, -105.075066], [40.014984, -105.270546]]:
     fg.add_child(folium.Marker(location = coordinates, popup = "Location", icon = folium.Icon(color = 'black')))
 #fg.add_child(folium.Marker(location=[40.40, -104.73], popup = "Greeley", icon = folium.Icon(color = 'beige')))
 #fg.add_child(folium.Marker(location=[39.7392, -104.99], popup = "Denver", icon = folium.Icon(color = 'black')))
