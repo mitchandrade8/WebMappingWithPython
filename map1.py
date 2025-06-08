5595b9ba8538d235878bf424746ef68621f3c391
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2025-06-08 14:27:51 -0600

    added a new json file

--- map1.py
+++ map1.py
@@ -27,6 +27,9 @@ for lt, ln, el in zip(lat, lon, elev):
    # fg.add_child(folium.Marker(location = coordinates, popup = "Location", icon = folium.Icon(color = 'black')))
 #fg.add_child(folium.Marker(location=[40.40, -104.73], popup = "Greeley", icon = folium.Icon(color = 'beige')))
 #fg.add_child(folium.Marker(location=[39.7392, -104.99], popup = "Denver", icon = folium.Icon(color = 'black')))
+
+fg.add_child(folium.GeoJson())
+
 map.add_child(fg)
 
 map.save("Map1.html")
